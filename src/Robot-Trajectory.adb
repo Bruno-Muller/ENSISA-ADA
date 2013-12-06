7040aa7209535c0d0688cdeaf13677ca737ad9ab
Bruno Muller <muller.bru@gmail.com>
2013-12-06 11:40:34 +0100

    Update Robot.Trajectory

--- src/Robot-Trajectory.adb
+++ src/Robot-Trajectory.adb
@@ -2,32 +2,45 @@ package body Robot.Trajectory is
 
    function Route(T: in Object) return Path.Object is
    begin
-      return Path.Null_Path;
+      return T.Route;
    end Route;
 
-   procedure Open(T: Object; P: in Path.Object; Speed: in Float) is
+   procedure Open(T: in out Object; P: in Path.Object; Speed: in Float) is
    begin
-      null;
+      T.Route := P;
+      T.Speed := Speed;
    end Open;
 
    function X(T: in Object) return Float is
    begin
-      return 0.0;
+      return Path.X(Path => T.Route,
+                    Segment => T.Segment,
+                    K => T.K);
    end X;
 
    function Y(T: in Object) return Float is
    begin
-      return 0.0;
+      return Path.Y(Path => T.Route,
+                    Segment => T.Segment,
+                    K => T.K);
    end Y;
 
-   procedure Next(T: Object; DeltaT: in Float) is
+   procedure Next(T: in out Object; DeltaT: in Float) is
+      DeltaK: Float := T.Speed/Path.Segment_Length(Path => T.Route, Segment => T.Segment)*DeltaT;
    begin
-      null;
+      T.K := T.K + DeltaK;
+      if T.K>1.0 then
+         T.K := 0.0;
+         T.Segment := T.Segment + 1;
+         if T.Segment>Path.Segment_Count(Path => T.Route) then
+            T.At_End := True;
+         end if;
+      end if;
    end Next;
 
    function At_End(T: in Object) return Boolean is
    begin
-      return False;
+      return T.At_End;
    end At_End;
 
    procedure Close is
--- src/Robot-Trajectory.ads
+++ src/Robot-Trajectory.ads
@@ -5,17 +5,21 @@ package Robot.Trajectory is
    type Object is tagged private;
 
    function Route(T: in Object) return Path.Object;
-   procedure Open(T: Object; P: in Path.Object; Speed: in Float);
+   procedure Open(T: in out Object; P: in Path.Object; Speed: in Float);
    function X(T: in Object) return Float;
    function Y(T: in Object) return Float;
-   procedure Next(T: Object; DeltaT: in Float);
+   procedure Next(T: in out Object; DeltaT: in Float);
    function At_End(T: in Object) return Boolean;
 
    procedure Close;
 
 private
    type Object is tagged record
-      null;
+      Route: Path.Object;
+      Speed: Float := 0.0;
+      Segment: Positive := 1;
+      K: Float := 0.0;
+      At_End: Boolean := False;
    end record;
 
 end Robot.Trajectory;
