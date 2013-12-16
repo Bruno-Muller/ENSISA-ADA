3923d24c1b7e691ded7552a8ea0f1a4de8791ce8
Bruno Muller <muller.bru@gmail.com>
2013-12-16 01:59:55 +0100

    Update Agency

--- src/Agency.adb
+++ src/Agency.adb
@@ -1,10 +1,12 @@
+with Robot;
+
 package body Agency is
 
    procedure Handle_Transfer(From: Site.Input_Places; To: Site.Output_Places) is
+   	Id: Robot.Robot_Id;
    begin
-
-      null; -- todo
-
+      Agency_Parking.Take(Id);
+      Agency_Robot_Table(Id).Go(From => From, To   => To);
    end Handle_Transfer;
 
    procedure Shutdown is
--- src/Agency.ads
+++ src/Agency.ads
@@ -1,6 +1,7 @@
 with Robot;
 with Adagraph;
 with Site;
+with Parking;
 
 package Agency is
 
@@ -10,12 +11,14 @@ package Agency is
 
 private
 
-   type Robot_Table is array(Integer range <>) of access Robot.Object;
+   type Robot_Table is array(Robot.Robot_Id range <>) of access Robot.Object;
 
-   Agency_Robot_Table: Robot_Table(1..6) := (new Robot.Object,
-                                             new Robot.Object(1, null, Adagraph.Blue),
-                                             new Robot.Object(2, null, Adagraph.Red),
-                                             new Robot.Object(3, null,  Adagraph.Cyan),
-                                             new Robot.Object(4, null, Adagraph.Magenta),
-                                             new Robot.Object(5, null, Adagraph.Yellow));
+   Agency_Parking: Parking.Object;
+   Agency_Robot_Table: Robot_Table(1..6) := (new Robot.Object(1, null, Adagraph.Green),
+                                             new Robot.Object(2, null, Adagraph.Blue),
+                                             new Robot.Object(3, null, Adagraph.Red),
+                                             new Robot.Object(4, null,  Adagraph.Cyan),
+                                             new Robot.Object(5, null, Adagraph.Magenta),
+                                             new Robot.Object(6, null, Adagraph.Yellow));
+
 end Agency;
