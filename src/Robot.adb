65e593c5208db0ae8db9aadf0bd6341b1d372ecb
Bruno Muller <muller.bru@gmail.com>
2013-12-17 14:05:30 +0100

    Update Robot

--- src/Robot.adb
+++ src/Robot.adb
@@ -15,31 +15,45 @@ package body Robot is
             when State=Ready => accept Go(From: Site.Input_Places; To: Site.Output_Places) do
                   State := Following;
 
+                  -- Ouvre un chemin (procédure bloquante), en fait on n'acquière que la place d'entrée ici
                   Trajectory.Safe.Open(Safe_Trajectory => Its_Trajectory,
                                        From => From,
                                        To => To,
                                        Speed => 75.0);
                end Go;
 
+               -- comme le code est synchronisé il faut mettre la boucle de déplacement à l'extérieur du accept pour avoir plusieurs robots simultanéement
+
+               -- Dessine le robot sur sa place d'entrée
                Site.Safely.Draw_Robot(Pnt    =>  Trajectory.XY(Trajectory.Safe.Get_Trajectory(Its_Trajectory)),
                                       Radius => Radius,
                                       Direction    => Trajectory.Direction(Trajectory.Safe.Get_Trajectory(Its_Trajectory)),
                                       Mouth_Opened    => True,
                                       Clr    => Color);
 
+               -- Le robot est sur la place d'entrée en attente d'acquérir le reste des ressources
+               -- Pas d'opération de dessins nécessaire
                Next := Ada.Calendar.Clock;
                while Trajectory.Safe.Is_On_Departure(Its_Trajectory) loop
+                  -- comme les tasks sont préemptées il faut comprendre que delay 0.5 va durer plus longtemps que 0.5s
+                  -- il faut gérer le delay par raport à l'horloge du système (faire un delay until Next; et pas un delay 0.5)
                   Next := Next + 0.05;
                   delay until Next;
+                  -- On ne déplace pas le robot durant cette phase d'attente
+                  -- mais on le titille pour qui essaie d'acquire le reste des ressources
                   Trajectory.Safe.Next(Safe_Trajectory => Its_Trajectory, DeltaT => 0.0);
                end loop;
 
+               -- Le robot est maintenant près à se déplacer
+               -- Il vient d'acquérir toutes les ressources
+               -- On dessine sont chemin
                Site.Safely.Draw_Path(Pth => Trajectory.Safe.Get_Trajectory(Its_Trajectory).Route,
                                      Clr => Color);
 
                Next := Ada.Calendar.Clock;
                while not Trajectory.At_End(Trajectory.Safe.Get_Trajectory(Its_Trajectory)) loop
                   Next := Next + 0.05;
+                  -- On dessine le robot là où il se trouve
                   Site.Safely.Draw_Robot(Pnt    =>  Trajectory.XY(Trajectory.Safe.Get_Trajectory(Its_Trajectory)),
                                          Radius => Radius,
                                          Direction    => Trajectory.Direction(Trajectory.Safe.Get_Trajectory(Its_Trajectory)),
@@ -47,26 +61,33 @@ package body Robot is
                                          Clr    => Color);
 
 
-
+                  -- Compteur utilisé pour fermer et ouvrir la bouche de Pacman
                   if Counter=7 then
                      Counter:=0;
                      Mouth_Opened := not Mouth_Opened;
                   end if;
-
                   Counter := Counter + 1;
 
+                  -- On attend 0.5s
                   delay until Next;
+
+                  -- On efface le robot
                   Site.Safely.Hide_Robot(Pnt    => Trajectory.XY(Trajectory.Safe.Get_Trajectory(Its_Trajectory)),
                                          Radius => Radius);
 
+                  -- On déplace le robot
                   Trajectory.Safe.Next(Safe_Trajectory => Its_Trajectory, DeltaT => 0.05);
 
 
                end loop;
 
+               -- Le robot est arrivé à destination, on ferme le chemin
                Trajectory.Safe.Close(Safe_Trajectory => Its_Trajectory);
 
+               -- Il passe à l'état Ready
                State := Ready;
+
+               -- On dépose le message de fin de mission dans la mailbox (Discrimant)
                if Mission_Done/=null then
                   Mission_Done.Put(Id);
                end if;
@@ -75,7 +96,7 @@ package body Robot is
                   State:=Shutdown;
                end Shutdown;
          end select;
-
+         -- ferme la tâche quand l'état est shutdown
          exit when State=Shutdown;
       end loop;
    end Object;
--- src/Robot.ads
+++ src/Robot.ads
@@ -6,9 +6,13 @@ with Generic_Mailbox;
 
 package Robot is
 
+   -- état du robot
    type State_Type is (Ready, Following, Shutdown);
+
+   -- id des robot, l'agence crée le nombre maximum de robot autorisé par range
    subtype Robot_Id is Natural range 1..6;
 
+   -- Mailbox dont les messages sont des id de robots
    package Robot_Mailbox is new Generic_Mailbox(Message => Robot_Id);
 
    task type Object(Id: Robot_Id := 1; Mission_Done: access Robot_Mailbox.Object := null; Color: Color_Type := Light_Green) is
@@ -17,6 +21,8 @@ package Robot is
    end Object;
 
    Radius: constant Float := 16.0;
+
+   -- Les couleurs des robots utilisées par l'agence et le parking
    Robot_Color: array (Robot_Id)  of Adagraph.Color_Type := (Adagraph.Green, Adagraph.Blue, Adagraph.Red, Adagraph.Cyan, Adagraph.Magenta, Adagraph.Yellow);
 
 end Robot;
