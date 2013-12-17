18971ca14735c9b3e33c70188876cd60bfd8b8e6
Bruno Muller <muller.bru@gmail.com>
2013-12-17 12:43:58 +0100

    Add comments to Generic_Resource_Pool

--- src/Generic_Resource_Pool.adb
+++ src/Generic_Resource_Pool.adb
@@ -1,21 +1,26 @@
 package body Generic_Resource_Pool is
 
+    -- Acquérir les ressources d'une requête
    procedure Acquire(Map : in Request_Map) is
    begin
+      -- On acquière les ressources demandées par la requête
       for Rsc in Resource_Id'First..Resource_Id'Last loop
          if Map(Rsc) then
-            Acquire(Rsc);
+            Acquire(Rsc); -- bloque si la ressource n'est pas disponible
          end if;
       end loop;
    end Acquire;
 
+   -- Acquérir une ressource en particulier
    procedure Acquire(Id: in Resource_Id) is
    begin
       Resource_Pool(Id).Acquire;
    end Acquire;
 
+   -- Libérer une requête de resource
    procedure Release(Map: in Request_Map) is
    begin
+      -- On libère les ressources de la requête
       for Rsc in Resource_Id'First..Resource_Id'Last loop
          if Map(Rsc) then
             Release(Rsc);
@@ -23,6 +28,7 @@ package body Generic_Resource_Pool is
       end loop;
    end Release;
 
+   -- Libérer une ressource en particulier
    procedure Release(Id: in Resource_Id) is
    begin
       Resource_Pool(Id).Release;
@@ -30,11 +36,13 @@ package body Generic_Resource_Pool is
 
    protected body Resource is
 
+      -- Libérer la ressource
       procedure Release is
       begin
          Acquired := False;
       end Release;
 
+      -- Acquirer la ressource, bloque si la ressource n'est pas disponible
       entry Acquire when not Acquired is
       begin
          Acquired := True;
--- src/Generic_Resource_Pool.ads
+++ src/Generic_Resource_Pool.ads
@@ -3,24 +3,38 @@ with Site;
 generic
    type Resource_Id is (<>);
 
+   -- Pool de resource générique
 package Generic_Resource_Pool is
 
+   -- Type utilisé pour représenter une requête d'accès aux ressources
    type Request_Map is array(Resource_Id) of Boolean;
 
+   -- Acquérir les ressources d'une requête
    procedure Acquire(Map : in Request_Map);
+
+   -- Acquérir une ressource en particulier
    procedure Acquire(Id: in Resource_Id);
+
+   -- Libérer une requête de resource
    procedure Release(Map: in Request_Map);
+
+   -- Libérer une ressource en particulier
    procedure Release(Id: in Resource_Id);
 
 private
 
+   -- type protéger pour réprésente une ressource
    protected type Resource is
+      -- Acquirer la ressource, bloque si la ressource n'est pas disponible
       entry Acquire;
+
+      -- Libérer la ressource
       procedure Release;
    private
       Acquired: Boolean := False;
    end;
 
+   -- Pool de ressource générique
    Resource_Pool : array(Resource_Id) of Resource;
 
 end Generic_Resource_Pool;
