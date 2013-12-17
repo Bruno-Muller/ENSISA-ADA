2424568f3b321a15bdc59ed308d664089a8034f2
Bruno Muller <muller.bru@gmail.com>
2013-12-17 13:20:25 +0100

    Add comments to Place_Randomizer

--- src/Place_Randomizer.adb
+++ src/Place_Randomizer.adb
@@ -1,6 +1,7 @@
 with Ada.Numerics.Discrete_Random;
 with Site;
 
+-- Randomizer de place d'entrée et de sortie
 package body Place_Randomizer is
 
    package Integer_Randomizer is new Ada.Numerics.Discrete_Random(Integer);
@@ -11,17 +12,27 @@ package body Place_Randomizer is
    Input_Generator: Input_Randomizer.Generator;
    Output_Generator: Output_Randomizer.Generator;
 
+   -- Obtient une place d'entée aléatoire
    function Random_Input return Site.Input_Places is
    begin
 	return Input_Randomizer.Random(Input_Generator);
    end Random_Input ;
 
+   -- Obtient une place de sortie aléatoire
    function Random_Output return Site.Output_Places is
    begin
 	return Output_Randomizer.Random(Output_Generator);
    end Random_Output ;
 
 begin
+
+   -- On initialise les génerateurs pseudo-aléatoires :
+
+   -- On s'assure que les générateurs de place d'entrée et de sortie
+   -- ont des seeds différents en prenant des seeds eux mêmes aléatoires.
+
+   -- Sinon les générateurs retourneraient les couples I1,01 puis I4,04 par exemple
+
    Integer_Randomizer.Reset(Gen => Integer_Generator);
 
    Input_Randomizer.Reset(Gen =>Input_Generator,
--- src/Place_Randomizer.ads
+++ src/Place_Randomizer.ads
@@ -1,8 +1,12 @@
 with Site;
 
+-- Randomizer de place d'entrée et de sortie
 package Place_Randomizer is
 
+   -- Obtient une place d'entée aléatoire
    function Random_Input return Site.Input_Places;
+
+   -- Obtient une place de sortie aléatoire
    function Random_Output return Site.Output_Places;
 
 end Place_Randomizer;
