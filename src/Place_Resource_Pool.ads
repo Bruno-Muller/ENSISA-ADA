5aeeca2ef971d27f7baaa73b7feacc8beceeb947
Bruno Muller <muller.bru@gmail.com>
2013-12-17 13:21:45 +0100

    Add comments to Place_Resource_Pool

--- src/Place_Resource_Pool.ads
+++ src/Place_Resource_Pool.ads
@@ -1,4 +1,5 @@
 with Generic_Resource_Pool;
 with Site;
 
+-- Un pool de places utilisant le pool générique
 package Place_Resource_Pool is new Generic_Resource_Pool(Resource_Id => Site.Place_Names);
